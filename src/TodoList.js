d7c110d8b62f131a2fc940e8f48d3c0fc2108a4a
Jason <1530665727@qq.com>
2019-12-24 20:24:14 +0800

    第一次提交

---
+++ src/TodoList.js
@@ -0,0 +1,37 @@
+import React,{Component, Fragment} from 'react';
+
+class TodoList extends Component{
+    constructor(props){
+        super();
+        this.state={
+            inputValue:'',
+            list:[]
+        }
+    }
+    handleInputChange = e =>{
+        this.setState({
+            inputValue:e.target.value,
+        })
+        // this.Setstate.inputValue = e.target.value;
+        console.log(e.target.value);
+    }
+    render() {
+        return(
+            <Fragment>
+                <div>
+                    <input
+                    value={this.state.inputValue}
+                    onChange={this.handleInputChange}
+                    />
+                    <button>提交</button>
+                </div>
+                <ul>
+                    <li>英语</li>
+                    <li>语文</li>
+                </ul>
+            </Fragment>
+            
+        )
+    }
+}
+export default TodoList;
